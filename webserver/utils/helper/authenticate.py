0d10853ea69476762dfa74955eab6dc472b85672
Giancarlo Zaniolo <gzaniolo@andrew.cmu.edu>
2024-11-06 11:59:10 -0500

    improved testing scripts

--- webserver/utils/helper/authenticate.py
+++ webserver/utils/helper/authenticate.py
@@ -4,8 +4,10 @@ from helper.send_to_api import send_login, send_register
 def register_and_login(server_url, uname, pw):
     token = send_login(server_url, uname, pw)
     if token == None:
-        if not send_register(server_url, uname, pw):
-            raise Exception("Fatal error could not register user")
+        # I think it is ok if the user already exists
+        send_register(server_url, uname, pw)
+        # if not send_register(server_url, uname, pw):
+            # raise Exception("Fatal error could not register user")
         token = send_login(server_url, uname, pw)
         if token == None:
             raise Exception("Fatal error could not login user after successful registration")
